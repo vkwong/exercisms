5039c592ae26f5c6a0baeaf83b33264b67237c49
vkwong <vkwong@andrew.cmu.edu>
2015-07-29 09:28:48 -0700

    Completed grade-school solution

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,26 +1,18 @@
 class School
+  def initialize
+    @db = Hash.new { |db, grade| db[grade] = [] }
+  end
 
-	attr_reader :name, :db
+  def add(student, grade)
+    @db[grade] << student
+  end
 
-	def new(name)
-		@name = name
-		@db ||= Hash.new { |students, grade| students[grade] = [] }
-	end
+  def grade(level)
+    @db[level].sort
+  end
 
-	def add(student, grade)
-		db[grade] << student
-	end
-
-	def grade(level)
-		db[level] || []
-	end
-
-	def sort
-		sorted = db.map { |grade, students| [grade, students.sort] }
-		Hash[sorted]
-	end
-
-	def to_hash
-		@db
-	end
+  def to_hash
+    sorted = @db.map { |grade, students| [grade, students.sort] }.sort
+    Hash[sorted]
+  end
 end
