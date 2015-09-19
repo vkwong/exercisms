cfe7f11d82323a14cbc2038d6fc8749c80a06681
Vivian Wong <vkwong@wustl.edu>
2015-09-19 18:42:45 -0500

    Competed nucleotide-count java solution and fetched word-count java exercism

--- java/nucleotide-count/src/main/java/DNA.java
+++ java/nucleotide-count/src/main/java/DNA.java
@@ -1,2 +1,36 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class DNA {
+    String dnaString;
+    String reqDNA = "ACGT";
+
+    public DNA(String str){
+        dnaString = str;
+    }
+
+    public int count(char needle){
+        int count = 0;
+        if (reqDNA.indexOf(needle) < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        for (char c : dnaString.toCharArray()) {
+            if (c == needle) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Map<Character, Integer> nucleotideCounts(){
+        Map<Character, Integer> dnaMap = new HashMap<Character, Integer>();
+
+        dnaMap.put('A', count('A'));
+        dnaMap.put('C', count('C'));
+        dnaMap.put('G', count('G'));
+        dnaMap.put('T', count('T'));
+
+        return dnaMap;
+    }
 }
