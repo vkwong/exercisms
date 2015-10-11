7e152a26c05a9a6aa30a68074e59d0510419edce
Vivian Wong <vkwong@wustl.edu>
2015-10-11 15:28:31 -0500

    Completed atbash_cipher.rb solution

--- ruby/atbash-cipher/atbash_cipher_test.rb
+++ ruby/atbash-cipher/atbash_cipher_test.rb
@@ -10,38 +10,38 @@ class AtbashTest < Minitest::Test
   end
 
   def test_encode_yes
-    skip
+    #skip
     assert_equal 'bvh', Atbash.encode('yes')
   end
 
   def test_encode_OMG
-    skip
+    #skip
     assert_equal 'lnt', Atbash.encode('OMG')
   end
 
   def test_encode_O_M_G
-    skip
+    #skip
     assert_equal 'lnt', Atbash.encode('O M G')
   end
 
   def test_encode_long_word
-    skip
+    #skip
     assert_equal 'nrmwy oldrm tob', Atbash.encode('mindblowingly')
   end
 
   def test_encode_numbers
-    skip
+    #skip
     assert_equal('gvhgr mt123 gvhgr mt',
                  Atbash.encode('Testing, 1 2 3, testing.'))
   end
 
   def test_encode_sentence
-    skip
+    #skip
     assert_equal 'gifgs rhurx grlm', Atbash.encode('Truth is fiction.')
   end
 
   def test_encode_all_the_things
-    skip
+    #skip
     plaintext = 'The quick brown fox jumps over the lazy dog.'
     cipher = 'gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt'
     assert_equal cipher, Atbash.encode(plaintext)
