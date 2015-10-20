9c60b26575587d7e7528e26372391e64d9f128e8
Vivian Wong <vkwong@wustl.edu>
2015-10-20 10:38:51 -0500

    Completed house ruby exercism

--- ruby/house/house.rb
+++ ruby/house/house.rb
@@ -1,30 +1,32 @@
 class House
-
-  VERSE_MAPPING = {
-    12=> {subject: 'horse and the hound and the horn', verb: 'belonged to' },
-    11=> {subject: 'farmer sowing his corn',           verb: 'kept' },
-    10=> {subject: 'rooster that crowed in the morn',  verb: 'woke' },
-    9 => {subject: 'priest all shaven and shorn',      verb: 'married' },
-    8 => {subject: 'man all tattered and torn',        verb: 'kissed' },
-    7 => {subject: 'maiden all forlorn',               verb: 'milked' },
-    6 => {subject: 'cow with the crumpled horn',       verb: 'tossed' },
-    5 => {subject: 'dog',                              verb: 'worried' },
-    4 => {subject: 'cat',                              verb: 'killed' },
-    3 => {subject: 'rat',                              verb: 'ate' },
-    2 => {subject: 'malt',                             verb: 'lay in' },
-    1 => {subject: 'house',                            verb: 'Jack built' },
-  }
-
-  def verse(num)
-    lines = num.downto(1).map do |n|
-      verse_parts = VERSE_MAPPING[n]
-      "#{verse_parts[:subject]} that #{verse_parts[:verb]}"
-    end
-    "This is the #{lines.join(' the ')}.\n"
+  def self.recite
+    new.recite
   end
 
-  def verses(from, to)
-    ( from.upto(to).map { |n| verse(n) }.join "\n" ) + "\n"
+  def recite
+    (1..pieces.length).map { |i| line(i) }.join("\n")
   end
 
+  def line(i)
+    "This is %s.\n" % pieces.last(i).map { |s| s.join("\n") }.join(' ')
+  end
+
+  private
+
+  def pieces
+    [
+      ['the horse and the hound and the horn', 'that belonged to'],
+      ['the farmer sowing his corn', 'that kept'],
+      ['the rooster that crowed in the morn', 'that woke'],
+      ['the priest all shaven and shorn', 'that married'],
+      ['the man all tattered and torn', 'that kissed'],
+      ['the maiden all forlorn', 'that milked'],
+      ['the cow with the crumpled horn', 'that tossed'],
+      ['the dog', 'that worried'],
+      ['the cat', 'that killed'],
+      ['the rat', 'that ate'],
+      ['the malt', 'that lay in'],
+      ['the house that Jack built']
+    ]
+  end
 end
